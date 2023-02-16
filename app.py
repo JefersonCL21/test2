6de4a4611e9bf748845fa0fbc739e6b06101fc5c
JefersonCL21 <jeferson@courageousland.com>
2023-02-15 22:12:06 -0300

    init commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+###################################
+
+
+def _max_width_():
+    max_width_str = f"max-width: 1800px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+st.set_page_config(page_icon="✂️", page_title="CSV Wrangler")
+
+st.title("CSV Wrangler")
